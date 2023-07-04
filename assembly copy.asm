65a0f641236d43cdab1d8e44ccefb732f8c4ce67
Pablo Pinheiro Assunção <pabloassuncao2@gmail.com>
2023-07-04 09:09:58 -0300

    Adiciona passagem de parâmetro por pilha

--- assembly copy.asm
+++ assembly copy.asm
@@ -1,9 +1,9 @@
 .data
 result_msg: .asciiz "A média aritmética é: "
-matriz: .word 1, 2, 3, 4, 5, 6, 7, 8                 # Matriz row-major order
-n: .word 2                                              # Tamanho da matriz
-m: .word 4                                              # Tamanho da matriz
-x: .word 0                                              # Número da linha i para calcular a média
+matriz: .word 1, 2, 3, 4, 5, 6, 7, 8, 9                 # Matriz row-major order
+n: .word 3                                              # Tamanho da matriz
+m: .word 3                                              # Tamanho da matriz
+x: .word 1                                              # Número da linha i para calcular a média
 y: .word 1                                              # Número da coluna j para calcular a média
 
 .text
@@ -42,10 +42,23 @@ main:
     jal     get_pos
     add     $s3,            $s3,        $v0             # $s3 = soma + valor da posição válida
     add     $s4,            $s4,        $v1             # $s4 = contador + posição válida
+    # Save parameters on the stack
+    subu    $sp,            $sp,        8               # Subtract 8 bytes from stack pointer
 
-# Realiza a divisão da soma pelo contador em ponto flutuante
-    mtc1    $s3,            $f4                         # $f0 = soma
-    mtc1    $s4,            $f2                         # $f1 = contador
+    sw      $s3,            0($sp)                      # Store $s3 on the stack
+    sw      $s4,            4($sp)                      # Store $s4 on the stack
+    j       calc_and_print
+
+
+calc_and_print:
+    # Access parameters in the callee
+    lw      $t0,            0($sp)                      # Load $s3 from the stack into $t0
+    lw      $t1,            4($sp)                      # Load $s4 from the stack into $t1
+    # Clean up the stack
+    addu    $sp,            $sp,        8               # Add 8 bytes back to the stack pointer
+    # Realiza a divisão da soma pelo contador em ponto flutuante
+    mtc1    $t0,            $f4                         # $f0 = soma
+    mtc1    $t1,            $f2                         # $f1 = contador
     cvt.s.w $f4,            $f4                         # $f0 = soma
     cvt.s.w $f2,            $f2                         # $f1 = contador
     div.s   $f6,            $f4,        $f2             # $f0 = soma / contador
@@ -60,7 +73,7 @@ main:
     syscall 
 
 get_pos:
-                                                        # Salva o primeiro endereço de retorno para a main antes de chamar outra função
+    # Salva o primeiro endereço de retorno para a main antes de chamar outra função
     sw      $ra,            0($sp)                      # Salva o endereço de retorno para a main
     addi    $sp,            $sp,        -4              # Decrementa o ponteiro da pilha
     move    $a2,            $s1                         # $a2 = n
@@ -79,13 +92,13 @@ get_pos:
     jr      $ra                                         # Retorna para a função chamadora
 
 check_pos:
-                                                        # Salva o primeiro endereço de retorno para a validate antes de chamar outra função
+    # Salva o primeiro endereço de retorno para a validate antes de chamar outra função
     sw      $ra,            0($sp)                      # Salva o endereço de retorno para a main
     addi    $sp,            $sp,        -4              # Decrementa o ponteiro da pilha
     jal     check_x_value
     move    $t2,            $v0                         # $t2 = As duas são verdadeiras para x
     jal     check_y_value
-                                                        # Atualiza o endereço de retorno para a main
+    # Atualiza o endereço de retorno para a main
     lw      $ra,            4($sp)                      # Carrega o endereço de retorno para a main
     addi    $sp,            $sp,        4               # Incrementa o ponteiro da pilha
     move    $t3,            $v0                         # $t3 = As duas são verdadeiras para y

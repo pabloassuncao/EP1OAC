7151f878d9fe864cfe2d133b193c76a81ace1d17
Pablo Pinheiro Assunção <pabloassuncao2@gmail.com>
2023-07-06 19:09:30 -0300

    Termina os requisitos do Código em Assembly

--- assembly copy.asm
+++ assembly copy.asm
@@ -1,5 +1,6 @@
 .data
-result_msg: .asciiz "A média aritmética é: "
+new_line: .asciiz "\n"                                  # String para imprimir uma nova linha
+space: .asciiz " "                                      # String para imprimir um espaço
 matriz: .word 1, 2, 3, 4, 5, 6, 7, 8, 9                 # Matriz row-major order
 n: .word 3                                              # Tamanho da matriz
 m: .word 3                                              # Tamanho da matriz
@@ -12,13 +13,41 @@ y: .word 0                                              # Número da coluna j pa
 
 main:
     la      $s0,            matriz                      # $s0 = matriz
-    lw      $s1,            n                           # $s1 = n linhas
-    lw      $s2,            m                           # $s2 = m colunas
+    lw      $s1,            n                           # $s1 = n
+    lw      $s2,            m                           # $s2 = m
+    li      $t8,            0                           # $t8 = row
+    li      $t9,            0                           # $t9 = column
+
+# Outer loop (rows)
+outer_loop:
+    inner_loop:
+        move    $a0,            $t8                         # $a0 = x
+        move    $a1,            $t9                         # $a1 = y
+        jal     adj_average
+        addi    $t9,            $t9,        1               # column = column + 1
+        bne     $t9,            $s2,        inner_loop      # Valida se a coluna é menor que m
+    
+        li      $t9,            0                           # column = 0
+        addi    $t8,            $t8,        1               # row = row + 1
+
+        li      $v0,            4                           # $v0 = 4
+        la      $a0,            new_line                    # $a0 = new_line
+        syscall                                             # Imprime uma nova linha
+
+        bne     $t8,            $s1,        outer_loop      # Valida se a linha é menor que n
+
+        li      $v0,            10                          # $v0 = 10
+        syscall                                             # Termina o programa
+
+adj_average:
+                                                        # Salva o primeiro endereço de retorno para a main antes de chamar outra função
+    sw      $ra,            0($sp)                      # Salva o endereço de retorno para a main
+    addi    $sp,            $sp,        -4              # Decrementa o ponteiro da pilha
     li      $s3,            0                           # $s3 = soma
     li      $s4,            0                           # $s4 = contador
     li      $s5,            0                           # $s5 = média
-    lw      $s6,            x                           # $s6 = x
-    lw      $s7,            y                           # $s7 = y
+    move    $s6,            $a0                         # $s6 = x
+    move    $s7,            $a1                         # $s7 = y
     addi    $a0,            $s6,        -1              # $s6 = x - 1
     move    $a1,            $s7                         # $s7 = y
     jal     get_pos
@@ -42,12 +71,17 @@ main:
     jal     get_pos
     add     $s3,            $s3,        $v0             # $s3 = soma + valor da posição válida
     add     $s4,            $s4,        $v1             # $s4 = contador + posição válida
-                                                        # Save parameters on the stack
-    subu    $sp,            $sp,        8               # Subtract 8 bytes from stack pointer
+                                                        # Salva os parâmetros na pilha
+    addi    $sp,            $sp,        -8              # Subtrai 8 bytes do ponteiro da pilha
 
-    sw      $s3,            0($sp)                      # Store $s3 on the stack
-    sw      $s4,            4($sp)                      # Store $s4 on the stack
-    j       calc_and_print
+    sw      $s3,            0($sp)                      # Salva $s3 na pilha
+    sw      $s4,            4($sp)                      # Salva $s4 na pilha
+                                                        # Chama a função calc_and_print
+    jal     calc_and_print
+
+    lw      $ra,            4($sp)                      # Carrega o endereço de retorno para a main
+    addi    $sp,            $sp,        4               # Incrementa o ponteiro da pilha
+    jr      $ra                                         # Retorna para a main
 
 
 calc_and_print:
@@ -55,7 +89,7 @@ calc_and_print:
     lw      $t0,            0($sp)                      # Load $s3 from the stack into $t0
     lw      $t1,            4($sp)                      # Load $s4 from the stack into $t1
                                                         # Clean up the stack
-    addu    $sp,            $sp,        8               # Add 8 bytes back to the stack pointer
+    addi    $sp,            $sp,        8               # Add 8 bytes back to the stack pointer
                                                         # Realiza a divisão da soma pelo contador em ponto flutuante
     mtc1    $t0,            $f4                         # $f0 = soma
     mtc1    $t1,            $f2                         # $f1 = contador
@@ -67,21 +101,21 @@ calc_and_print:
     mtc1    $t0,            $f2                         # $f2 = 10
     cvt.s.w $f2,            $f2                         # $f2 = 10
     mul.s   $f6,            $f6,        $f2             # $f0 = soma * 10
-    # make the float with only 2 decimal places
-    trunc.w.s $f6,          $f6                         # $f0 = soma * 10
+                                                        # make the float with only 2 decimal places
+    trunc.w.s $f6,            $f6                        # $f0 = soma * 10
     cvt.s.w $f6,            $f6                         # $f0 = soma * 10
     div.s   $f6,            $f6,        $f2             # $f0 = soma * 10 / 10
-
-    la      $a0,            result_msg                  # $a0 = "A média aritmética é: "
-    li      $v0,            4                           # $v0 = print_string
-    syscall 
 
 # Convert the float number to a string
     li      $v0,            2                           # $v0 = print_float
     mov.s   $f12,           $f6                         # $f12 = $f6
-    syscall 
-    li      $v0,            10                          # $v0 = exit
-    syscall 
+    syscall
+
+    li      $v0,            4                           # $v0 = print_string
+    la      $a0,            space                       # $a0 = space
+    syscall
+
+    jr      $ra                                         # Return to the caller
 
 get_pos:
                                                         # Salva o primeiro endereço de retorno para a main antes de chamar outra função
@@ -121,9 +155,9 @@ check_x_value:
     li      $t2,            0                           # $t2 = As duas são verdadeiras para x
     li      $t0,            0                           # $t0 = Primeira comparação
     li      $t1,            0                           # $t1 = Segunda comparação
-    addi    $t8,            $zero,      -1              # $t8 = -1
+    addi    $t6,            $zero,      -1              # $t6 = -1
     slt     $t0,            $a0,        $a2             # $t0 = x < n
-    sgt     $t1,            $a0,        $t8             # $t1 = x > -1
+    sgt     $t1,            $a0,        $t6             # $t1 = x > -1
     and     $t2,            $t0,        $t1             # $t2 = $t0 == $t1
     move    $v0,            $t2                         # $v0 = $t2
     jr      $ra                                         # Retorna para a função chamadora
@@ -132,17 +166,17 @@ check_y_value:
     li      $t0,            0                           # $t0 = Primeira comparação
     li      $t1,            0                           # $t1 = Segunda comparação
     li      $t3,            0                           # $t3 = As duas são verdadeiras para y
-    addi    $t8,            $zero,      -1              # $t8 = -1
+    addi    $t6,            $zero,      -1              # $t6 = -1
     slt     $t0,            $a1,        $a3             # $t0 = y < n
-    sgt     $t1,            $a1,        $t8             # $t1 = y > -1
+    sgt     $t1,            $a1,        $t6             # $t1 = y > -1
     and     $t3,            $t0,        $t1             # $t3 = $t0 == $t1
     move    $v0,            $t3                         # $v0 = $t3
     jr      $ra                                         # Retorna para a função chamadora
 
 count_pos:
-    lw      $t7,            m                           # $t7 = m
+    lw      $t6,            m                           # $t6 = m
     li      $t5,            4                           # $t5 = Tamnho de uma palavra
-    mulo    $t6,            $a0,        $t7             # $t6 = x * m
+    mulo    $t6,            $a0,        $t6             # $t6 = x * m
     add     $t6,            $t6,        $a1             # $t6 = x * m + y
     mulo    $t6,            $t6,        $t5             # $t6 = (x * m + y) * 4
     add     $t6,            $a2,        $t6             # $t6 = matriz + (x * m + y) * 4

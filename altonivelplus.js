a430a146253545433e2e7dfc24974148e560c531
Pablo Pinheiro Assunção <pabloassuncao2@gmail.com>
2023-07-06 19:59:15 -0300

    Update do código abstrato

--- altonivelplus.js
+++ altonivelplus.js
@@ -1,18 +1,18 @@
-function adjAverage(matrix, n, m) {
-  const resp = new Array(n).fill(0).map(() => []);
+function adjAverage(mat, n, m) {
+  const r = new Array(n).fill(0).map(() => []);
   
   for (let x = 0; x < n; x++) {
     for (let y = 0; y < m; y++) {
-      resp[x].push(Math.floor([[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]].filter((ele) => (ele[0] >= 0 && ele[0] < matrix.length) && (ele[1] >= 0 && ele[1] < matrix[0].length)).reduce((acc, e, _i, arr) => acc+(matrix[e[0]][e[1]]/arr.length), 0)*10)/10);
+      r[x].push(Math.floor([[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]].filter((e) => (e[0] >= 0 && e[0] < mat.length) && (e[1] >= 0 && e[1] < mat[0].length)).reduce((a, c, _i, arr) => a+(mat[c[0]][c[1]]/arr.length), 0)*10)/10);
     }
   }
-  return resp;
+  return r;
 }
 
-const matrix = [
+const mat = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9],
 ];
 
-adjAverage(matrix, 3, 3);
+console.log(adjAverage(mat, 3, 3));

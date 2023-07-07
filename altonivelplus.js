6723ee5d3da29edac369dd3e62eaf84af2e68991
Pablo Pinheiro Assunção <pabloassuncao2@gmail.com>
2023-07-07 11:23:05 -0300

    Edita o código alto nível

--- altonivelplus.js
+++ altonivelplus.js
@@ -1,10 +1,5 @@
 function adjAverage(mat, n, m) {
-  for (let x = 0; x < n; x++) {
-    for (let y = 0; y < m; y++) {
-      process.stdout.write((Math.floor([[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]].filter((e) => (e[0] >= 0 && e[0] < n) && (e[1] >= 0 && e[1] < m)).reduce((a, c, _i, arr) => a+(mat[c[0]][c[1]]/arr.length), 0)*10)/10).toFixed(1) + " ");
-    }
-    process.stdout.write("\n");
-  }
+  return new  Array(n).fill(new Array(m).fill(0)).map((e, x, arr) => e.map((e, y) => (Math.floor([[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]].filter((e) => (e[0] >= 0 && e[0] < n) && (e[1] >= 0 && e[1] < m)).reduce((a, c, _i, arr) => a+(mat[c[0]][c[1]]/arr.length), 0)*10)/10).toFixed(1)));
 }
 
 const mat = [
@@ -13,4 +8,4 @@ const mat = [
   [7, 8, 9],
 ];
 
-adjAverage(mat, 3, 3);
+console.log(adjAverage(mat, 3, 3));
